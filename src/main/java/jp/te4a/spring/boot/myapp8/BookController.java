ed5eedb2185190fe90cb5fccf1d36734c870a717
200714 <200714@e0601-11.JC-21.local>
2023-06-01 11:58:32 +0900

    a

--- src/main/java/jp/te4a/spring/boot/myapp8/BookController.java
+++ src/main/java/jp/te4a/spring/boot/myapp8/BookController.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp6;
+package jp.te4a.spring.boot.myapp8;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
-public class HelloController {
+public class BookController {
 	@Autowired
 	BookService bookService;
 	@RequestMapping("books/list")
--- src/main/java/jp/te4a/spring/boot/myapp8/BookRepository.java
+++ src/main/java/jp/te4a/spring/boot/myapp8/BookRepository.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp6;
+package jp.te4a.spring.boot.myapp8;
 
 import java.util.List;
 import java.util.concurrent.ConcurrentMap;
