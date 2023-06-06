064c9cad0698c8fb8a7be40ffe462601e7773dc9
200714 <200714@e0601-11.JC-21.local>
2023-06-06 11:12:24 +0900

    "完了"

--- src/main/java/jp/te4a/spring/boot/myapp8/BookApplication.java
+++ src/main/java/jp/te4a/spring/boot/myapp8/BookApplication.java
@@ -1,5 +1,6 @@
 package jp.te4a.spring.boot.myapp8;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/jp/te4a/spring/boot/myapp8/BookBean.java
+++ src/main/java/jp/te4a/spring/boot/myapp8/BookBean.java
@@ -1,9 +1,11 @@
 package jp.te4a.spring.boot.myapp8;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BookBean {
 	Integer id;
 	String title;
