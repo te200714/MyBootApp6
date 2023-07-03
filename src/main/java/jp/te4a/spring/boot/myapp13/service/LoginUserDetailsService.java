6762b68ba1d1054a6297ab6f753bd6c78ca12fa7
200714 <200714@e0601-11.JC-21.local>
2023-07-03 10:26:59 +0900

    途中

--- src/main/java/jp/te4a/spring/boot/myapp13/service/LoginUserDetailsService.java
+++ src/main/java/jp/te4a/spring/boot/myapp13/service/LoginUserDetailsService.java
@@ -1,6 +1,7 @@
 package jp.te4a.spring.boot.myapp13.service;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import 
@@ -15,10 +16,13 @@ public class LoginUserDetailsService implements UserDetailsService{
 	@Autowired
     UserRepository userRepository;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<UserBean> opt = userRepository.findById(username);
-        UserBean user = opt.orElseThrow(() -> new UsernameNotFoundException("The requested user is not found."));
-        return new LoginUserDetails(user);
+    public UserDetails loadUserByUsername(String username) throws 
+    UsernameNotFoundException {
+    	Optional<UserBean> opt = userRepository.findById(username);
+    	UserBean user = opt.orElseThrow(() -> new 
+    	UsernameNotFoundException("The requested user is not found."));
+    	return new LoginUserDetails(user);
+
     }
 
 }
